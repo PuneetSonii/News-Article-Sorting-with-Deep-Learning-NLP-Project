87c35b04ba7e644709090dd5040076da9ae7021f
PuneetSonii <puneetsonii369@gmail.com>
2024-03-26 19:27:26 +0530

    updated

---
+++ main.py
@@ -0,0 +1,3 @@
+from newsSorter.logging import logger
+
+logger.info("Welcome to our custom logging")
--- template.py
+++ template.py
@@ -5,7 +5,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 
-project_name = "NEWS SORTING"
+project_name = "newsSorter"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
